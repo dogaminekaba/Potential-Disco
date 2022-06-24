dca0cb46f1b2348456322a869078b9cdccd006c7
Dmk <dogaminekaba@users.noreply.github.com>
2022-06-24 10:51:40 +0200

    POD-22
    Initial Unreal project is created
    
    https://blu-red.atlassian.net/browse/POD-22

---
+++ Source/PotentialDisco/PotentialDisco.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PotentialDisco.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PotentialDisco, "PotentialDisco" );
---
+++ Source/PotentialDisco/PotentialDiscoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PotentialDiscoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POTENTIALDISCO_API APotentialDiscoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
