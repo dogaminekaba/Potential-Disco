4a8666d7a37675294200892392e21b4b1ed4152b
Dmk <dogaminekaba@users.noreply.github.com>
2022-08-02 14:33:46 +0200

    Added basic conttrollers

--- Source/PotentialDisco/ShooterCharacter.h
+++ Source/PotentialDisco/ShooterCharacter.h
@@ -19,11 +19,59 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	/// <summary>
+	/// Called for forwards/backwards input 
+	/// </summary>
+	/// <param name="Value"></param>
+	void MoveForward(float Value);
+
+	/// <summary>
+	/// Called for side to side input 
+	/// </summary>
+	/// <param name="Value"></param>
+	void MoveRight(float Value);
+
+	/// <summary>
+	/// Called via input to turn at a given rate
+	/// </summary>
+	/// <param name="Rate"> This is a normalized rate, i.e. 1.0 means 100% of desired turn rate </param>
+	void TurnAtRate(float Rate);
+
+	/// <summary>
+	/// Called via input to look up/down at a given rate
+	/// </summary>
+	/// <param name="Rate"> This is a normalized rate, i.e. 1.0 means 100% of desired turn rate </param>
+	void LookUpAtRate(float Rate);
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	/** Camera that follows the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	float BaseTurnRate;
+
+	/** Base look up/down rate, in deg/sec. Other scaling may affect final turn rate */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	float BaseLookUpRate;
+
+public:
+	/** Returns CameraBoom subobject */
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject */
+	FORCEINLINE UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
 
 };
